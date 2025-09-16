b045f236008dc185a93e8d47ded67da916553c1f
ChaudhariVarsha <126560621+ChaudhariVarsha@users.noreply.github.com>
2025-09-16 06:15:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "ap-south-1" # or your preferred region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
